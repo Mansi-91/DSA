85a29438ce3850c2a76b6229a409562c0bfc0cb7
Mansi Kapse <kapsemansi89@gmail.com>
2026-07-02 15:36:30 +0530

    Intersecting Node

--- LinkedList/IntersectionOfLL.java
+++ LinkedList/IntersectionOfLL.java
@@ -1,14 +1,14 @@
 package LinkedList;
 
 public class IntersectionOfLL {
-    public static int intersectionOfLL(Node n1, Node n2){
+    public static Node intersectionOfLL(Node n1, Node n2){
         Node temp1 = n1;
         Node temp2 = n2;
         while(temp1 != temp2){
             temp1 = (temp1 != null) ? temp1.next : n2;
             temp2 = (temp2 != null) ? temp2.next : n1;
         }
-        return (temp1 == null) ? -1 : temp1.data;
+        return temp1;
     }
     public static void main(String[] args){
         Node n1 = new Node(1);
@@ -18,8 +18,9 @@ public class IntersectionOfLL {
         n1.next.next.next.next = new Node(7);
         Node n2 = new Node(4);
         n2.next = new Node(5);
-        
-        System.out.println(intersectionOfLL(n1, n2));
+        Node ans = intersectionOfLL(n1, n2);
+        if(ans != null) System.out.println(ans.data);
+        else System.out.println("No intersection");
 
     }
 }
